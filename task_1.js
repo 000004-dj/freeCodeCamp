dcc66fa49ad138545d91364b050ef50d12a37ebe
Samuil <samuil-18@tut.by>
2022-10-07 21:31:50 +0200

    save only branch main

---
+++ task_1.js
@@ -0,0 +1,109 @@
+// it's my first comment 
+/* it's my second comment */
+function a(){
+    return 'hello'
+}
+
+// task 2 
+var myName;
+
+// task 3
+var a;
+a = 7;
+var b;
+
+//task 4 
+
+var a;
+a = 7;
+var b;
+b = a
+
+//task 5 
+
+var a = 9
+
+//task 6 
+
+var myFirstName = 'Samuil' 
+var myLastName = 'Prytchyn' 
+
+//task 7
+var a = 5;
+var b = 10;
+var c = 'I am a';
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//task 8
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+//task 9
+let catName = "Oliver";
+let catSound = "Meow!";
+//task 10 
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+//task 11
+const sum = 10 + 10;
+//task 12 
+const difference = 45 - 33;
+//task 13
+const product = 8 * 10;
+//task 14 
+const quotient = 66 / 33;
+//task 15 
+let myVar = 87;
+myVar++
+//task 16 
+let myVar = 11;
+myVar--
+//task 17 
+const ourDecimal = 5.7;
+let myDecimal = 3.14159
+//task 18 
+const product = 2.0 * 2.5;
+//task 19
+const quotient = 4.4 / 2.0; 
+//task 20 
+const remainder = 11 % 3
+console.log (remainder) 
+//task 21 
+let a = 3;
+let b = 17;
+let c = 12;
+a += 12;
+b += 9;
+c += 7;
+console.log(a,b,c)
+//task 22
+let a = 11;
+let b = 9;
+let c = 3;
+a -= 6;
+b -= 15;
+c -= 1;
+//task 23
+let a = 5;
+let b = 12;
+let c = 4.6;
+a *= 5;
+b *= 3;
+c *= 10;
+//task 24 
+let a = 48;
+let b = 108;
+let c = 33;
+a /= 12;
+b /= 4;
+c /= 11;
+//task 25
