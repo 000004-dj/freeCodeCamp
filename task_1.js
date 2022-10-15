b8e8971d707f33ffa58e107ca07808dd761fed66
Samuil <samuil-18@tut.by>
2022-10-15 20:48:30 +0200

    add new task

--- task_1.js
+++ task_1.js
@@ -172,3 +172,13 @@ function testNotEqual(val) {
   
   testNotEqual(10);
   //41
+  // Setup
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
+  //42
