482af643ee3841157c209acc58e6c5fdba5ee11f
Samuil <samuil-18@tut.by>
2022-10-15 20:52:43 +0200

    add new task

--- task_1.js
+++ task_1.js
@@ -196,3 +196,17 @@ function testStrictNotEqual(val) {
   
   testGreaterThan(10);
   //43
+  function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
+  //44
