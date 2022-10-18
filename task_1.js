c83d5b8314c6d4028295b77530148ea869d8656c
Samuil <samuil-18@tut.by>
2022-10-18 22:44:15 +0200

    add new task

--- task_1.js
+++ task_1.js
@@ -284,4 +284,32 @@ function testStrictNotEqual(val) {
   }
   
   testSize(7);
+  //49
+
+  function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta"
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  caseInSwitch(1);
+
+
+  //50
   
