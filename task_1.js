35fbacb92165f8730fcdc2ea324b2ef144a0a5b5
Samuil <samuil-18@tut.by>
2022-10-18 22:57:25 +0200

    add new task

--- task_1.js
+++ task_1.js
@@ -400,3 +400,15 @@ function testStrictNotEqual(val) {
   }
   
   chainToSwitch(7);
+
+  //53
+
+  function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
+
+  //54
